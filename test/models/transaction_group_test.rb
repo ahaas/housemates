e5facca4ada6debe8fc48fca7914bccbb9fc8ff1
tommot <tomlai@live.hk>
2014-10-22 13:46:07 -0700

    implement transaction group model

---
+++ test/models/transaction_group_test.rb
@@ -0,0 +1,45 @@
+# Tom Lai
+
+require 'test_helper'
+
+class TransactionGroupTest < ActiveSupport::TestCase
+def setup
+    @group = TransactionGroup.new(name:"test transaction", total_amount: 10.00)
+  end
+
+  test "should be valid" do
+    assert @group.valid?
+  end
+
+  test "name should be present" do
+  @group.name = nil
+  assert_not @group.valid?
+  end
+
+  test "empty name should not be valid" do
+  @group.name = ""
+  assert_not @group.valid?
+  end
+
+  test "total_amount should be present" do
+  @group.total_amount = nil
+  assert_not @group.valid?
+  end
+
+  test "negative total_amount should not be valid" do
+  @group.total_amount = -1.0
+  assert_not @group.valid?
+  end
+
+  test "zero total_amount should not be valid" do
+  @group.total_amount = 0.0
+  assert_not @group.valid?
+  end
+
+  test "total_amount should round off after save" do
+  @group.total_amount = 1.54
+  @group.save
+  @group.reload
+  assert @group.total_amount = 1.5
+  end
+end
