1121f531935dca7ab7310dab4644963e7fec2692
Opal Kale <opal.kale@gmail.com>
2014-11-05 19:31:00 -0800

    Add functional tests for finance, announcements pages

---
+++ test/integration/users_announcements_test.rb
@@ -0,0 +1,66 @@
+# Opal Kale
+require 'test_helper'
+
+class UsersAnnouncementsTest < ActionDispatch::IntegrationTest
+  
+  def setup
+  # Creates User #1 & User #2
+    @user1 = User.create(name:  "opal", email: "opal@example.com",
+                      password: "letmein", 
+                      password_confirmation: "letmein")
+    @user2 = User.create(name: "eric", email: "eric@example.com",
+                     password: "letmein", 
+                      password_confirmation: "letmein")
+
+    # Creates 'Stars' Household
+    @household1 = Household.create(name: 'stars')
+
+    # Adds User #1 & User #2 to 'Stars' Household
+    @user1.household = @household1
+    @user1.save
+
+    @user2.household = @household1
+    @user2.save
+
+    @user1_id = @user1.id
+    @user2_id = @user2.id
+
+    # Logs User #1 in
+    log_in_as @user1, password: "letmein"
+  end
+
+  test "show announcements" do
+    get announcements_show_path
+    assert_template 'announcements/show'
+  end
+
+  test "no announcements" do
+    get announcements_show_path
+    assert_select "tr", count: 0
+  end
+
+  test "new announcement" do
+    post_via_redirect announcements_create_path, commit: 'Post', text: 'Test announcement!'
+    assert_template 'announcements/show'
+    assert_select "tr", count: 1
+    assert_select "h2", text: 'Test announcement!'
+  end
+
+  test "empty announcement" do
+    post_via_redirect announcements_create_path, commit: 'Post'
+    assert_template 'announcements/show'
+    assert_select "tr", count: 0
+  end
+
+  test "delete announcement" do
+    # Creating a new announcement to delete
+    post_via_redirect announcements_create_path, commit: 'Post', text: 'Test announcement!', id: '1'
+    assert_select "h2", text: 'Test announcement!'
+    # Deleting that announcement
+    @announcement_id = Announcement.last.id
+    delete_via_redirect announcements_destroy_path, id: @announcement_id
+    assert_template 'announcements/show'
+    assert_select "tr", count: 1
+  end
+
+end
---
+++ test/integration/users_new_payback_test.rb
@@ -0,0 +1,57 @@
+# Opal Kale
+require 'test_helper'
+
+class UsersNewPaybackTest < ActionDispatch::IntegrationTest
+
+  def setup
+    # Creates User #1 & User #2
+    @user1 = User.create(name:  "opal", email: "opal@example.com",
+                      password: "letmein", 
+                      password_confirmation: "letmein")
+    @user2 = User.create(name: "eric", email: "eric@example.com",
+                     password: "letmein", 
+                      password_confirmation: "letmein")
+
+    # Creates 'Stars' Household
+    @household1 = Household.create(name: 'stars')
+
+    # Adds User #1 & User #2 to 'Stars' Household
+    @user1.household = @household1
+    @user1.save
+
+    @user2.household = @household1
+    @user2.save
+
+    @user1_id = @user1.id
+    @user2_id = @user2.id
+    # Creates balance for each user
+    Balance.create(user1: @user1, user2: @user2)
+
+    # Logs User #1 in
+    log_in_as @user1, password: "letmein"
+  end
+
+  test "get paid back button" do
+    get transactions_new_payback_path
+    assert_template 'transactions/new_payback'
+  end
+
+  test "num of rows generated = 3 before any transactions" do
+    get transactions_show_path
+    assert_select "tr", count: 3 # Rows for Balance Titles, Rows for 1 Housemate, and row for Transaction Titles.
+  end
+  
+  test "new payback item" do
+    #debugger
+    post_via_redirect transactions_create_path, amount: '10.0', commit: 'Pay back', user_id: @user2_id
+    assert_template 'transactions/show'
+    assert_select "tr", count: 4 # Additonal row generated for new transaction
+  end
+  
+  test "invalid amount flash message" do
+    post_via_redirect transactions_create_path, amount: '0.0', commit: 'Pay back', user_id: @user2_id
+    assert_template 'transactions/show'
+    assert_not flash.empty? 
+  end
+
+end
