70c528a026670d5cbf493c8b864f62e3e2e2b9cc
Opal Kale <opal.kale@gmail.com>
2014-11-22 12:59:51 -0800

    Code coverage 99%

--- test/integration/users_create_event_test.rb
+++ test/integration/users_create_event_test.rb
@@ -11,10 +11,11 @@ class UsersCreateEventTest < ActionDispatch::IntegrationTest
     @household1 = Household.create(name: 'Eventful')
     @user1.household = @household1
     @user1.save
+
+    log_in_as @user1, password: "daringdarius"
   end
 
   test "create event" do
-    log_in_as @user1, password: "daringdarius"
     assert_select "span[class=fc-title]", count: 0
     post_via_redirect events_create_path, name: "valid event", start_at: "11/7/2014 3:00 PM", end_at: "11/7/2014 4:00 PM"
     assert_template "events/show"
@@ -22,20 +23,70 @@ class UsersCreateEventTest < ActionDispatch::IntegrationTest
   end
 
   test "create event with empty name" do
-    log_in_as @user1, password: "daringdarius"
     post_via_redirect events_create_path, name: "", start_at: "11/7/2014 3:00 PM", end_at: "11/7/2014 4:00 PM"
     assert_template "events/new"
   end
 
   test "create event with empty start" do
-    log_in_as @user1, password: "daringdarius"
     post_via_redirect events_create_path, name: "no start", start_at: "", end_at: "11/7/2014 4:00 PM"
     assert_template "events/new"
   end
 
   test "create event with empty end" do
-    log_in_as @user1, password: "daringdarius"
     post_via_redirect events_create_path, name: "no start", start_at: "11/7/2014 3:00 PM", end_at: ""
     assert_template "events/new"
   end
+
+  test "renders new event page" do
+    get events_new_path
+    assert_template 'events/new'
+  end
+
+  test "edit unknown event" do
+    # Creating a new event
+    post_via_redirect events_create_path, name: "valid event", start_at: "11/7/2014 3:00 PM", end_at: "11/7/2014 4:00 PM"
+    @event1 = Event.last()
+    get events_edit_path, id: nil
+    assert_template 'events/show'
+    assert_not flash.empty?
+  end
+
+  test "renders edit event page" do
+    # Creating a new event
+    post_via_redirect events_create_path, name: "valid event", start_at: "11/7/2014 3:00 PM", end_at: "11/7/2014 4:00 PM"
+    @event1 = Event.last()
+    get events_edit_path, id: @event1.id
+    assert_template 'events/edit', id: @event1.id 
+  end
+
+  test "successfully delete event" do
+    # Creating a new event
+    post_via_redirect events_create_path, name: "valid event", start_at: "11/7/2014 3:00 PM", end_at: "11/7/2014 4:00 PM"
+    @event1 = Event.last()
+
+    post_via_redirect events_update_path(id: @event1.id, commit: 'Delete event', name: @event1.name, start_at: @event1.start_at, end_at: @event1.end_at)
+    assert_template 'events/show'
+  end
+
+  test "successfully update event" do
+    # Creating a new event
+    post_via_redirect events_create_path, name: "valid event", start_at: "11/7/2014 3:00 PM", end_at: "11/7/2014 4:00 PM"
+    @event1 = Event.last()
+
+    post_via_redirect events_update_path(id: @event1.id, commit: 'Save', name: "New event name", start_at: @event1.start_at, end_at: @event1.end_at)
+    assert_template 'events/show'
+    assert_not flash.empty?
+  end
+
+  test "unsuccessfully update event" do
+    # Creating a new event
+    post_via_redirect events_create_path, name: "valid event", start_at: "11/7/2014 3:00 PM", end_at: "11/7/2014 4:00 PM"
+    @event1 = Event.last()
+
+    post events_update_path(id: @event1.id, commit: 'Save', name: "New event name", start_at: "11/7/2014 4:00 PM", end_at: "11/7/2014 3:00 PM")
+    assert_template 'events/edit'
+    post_via_redirect events_update_path, id: nil
+    assert_template 'events/show'
+  end
+
 end
--- test/integration/users_reset_test.rb
+++ test/integration/users_reset_test.rb
@@ -10,17 +10,33 @@ class UsersResetTest < ActionDispatch::IntegrationTest
                       password_confirmation: "letmein")
   end
 
-  test "it creates a reset for a user with an email" do
-    post_via_redirect reset_create_path, email: @user1.email
+  test "no email in database" do
+    post_via_redirect reset_create_path, email: 'doesntexist@example.com'
     assert_template 'users/new'
   end
 
-  test "reset password" do
+  test "successful password reset" do
+    get forget_password_path
+    post_via_redirect reset_create_path, email: @user1.email
     get reset_path
-    assert_template 'users/reset'
-    post_via_redirect reset_path email: 'jackie@example.com', token: '123', password: 'password', password_confirmation: 'password'
+    @user1.reload
+    post_via_redirect reset_path, email: @user1.email, password: @user1.password, token: @user1.reset_digest
     assert_template 'users/new'
     assert_not flash.empty?
+  end
+
+  test "unsuccessful password reset" do
+    post_via_redirect reset_create_path, email: @user1.email
+    get reset_path
+    @user1.reload
+    post_via_redirect reset_path, email: @user1.email, password: @user1.password, token: 'incorrect token'
+    assert_template 'users/new'
+    assert_not flash.empty?
+  end
+
+  test "forget password form" do
+    get forget_password_path
+    assert_template 'users/forget_password'
   end
 
 end
